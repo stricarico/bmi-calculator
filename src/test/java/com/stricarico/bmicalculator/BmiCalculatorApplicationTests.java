7ccaff548625098666134a409a7334d4c3b1cdd5
BuildTools <unconfigured@null.spigotmc.org>
2022-06-23 19:51:00 -0300

    Initial commit

---
+++ src/test/java/com/stricarico/bmicalculator/BmiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stricarico.bmicalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
